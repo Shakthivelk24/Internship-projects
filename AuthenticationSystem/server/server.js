c39b7ce36eee2f29d5bb01228688f01d938045df
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-16 20:40:07 +0530

    add: configure CORS middleware for handling cross-origin requests

--- AuthenticationSystem/server/server.js
+++ AuthenticationSystem/server/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routers/auth.routes.js";
 import userRoutes from "./routers/user.routes.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -15,6 +16,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}))
+
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
