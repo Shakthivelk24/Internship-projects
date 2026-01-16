1c9e28b7622e77f9b975616f8c8e6fb29521b1b3
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-16 20:39:01 +0530

    refactor: replace placeholder with user authentication routes and context integration

--- AuthenticationSystem/client/src/App.jsx
+++ AuthenticationSystem/client/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import {useContext} from 'react'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import { Routes, Route } from 'react-router-dom'
+import { userDataContext } from './context/UserContext.jsx'
+import { Navigate } from 'react-router-dom';
+import { Toaster } from "react-hot-toast";
+import Home from './pages/Home.jsx'
 
 function App() {
+  const {userData,setUserData} = useContext(userDataContext);
   return (
-    <div className='text-center'>
-        hello world
-    </div>
+   <>
+    <Toaster />
+    <Routes>
+        <Route path="/" element={userData ? <Home /> : <Navigate to={"/signin"} />} />
+        <Route path="/signin" element={!userData ? <SignIn /> : <Navigate to={"/"} />} />
+        <Route path="/signup" element={!userData ? <SignUp /> : <Navigate to={"/"} />} />
+    </Routes> 
+   </>
   )
 }
 
