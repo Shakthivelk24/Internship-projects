62d55a0a189812cd14ea2d4762c05cecf376dd54
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-17 22:17:48 +0530

    add: initialize server with Express, CORS, and routing for authentication and user management

---
+++ BloggingPlatform/server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routers/auth.routes.js";
+import userRoutes from "./routers/user.routes.js";
+import cors from "cors";
+
+dotenv.config();
+
+connectDB();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}))
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
