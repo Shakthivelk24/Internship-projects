dbcb2ef2b018b7b55743a1657d3ff18544fb8ee1
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-18 21:27:08 +0530

    add: implement user data fetching in UserContext component so that the data present in cookies can be used

--- AuthenticationSystem/client/src/context/UserContext.jsx
+++ AuthenticationSystem/client/src/context/UserContext.jsx
@@ -7,6 +7,20 @@ export const userDataContext = createContext();
 function UserContext({ children }) {
   const serverUrl = "http://localhost:8000";
   const [userData, setUserData] = useState(null);
+  const handleCurrentUser = async () => {
+    try {
+      const result = await axios.get(`${serverUrl}/api/user/current`, {
+        withCredentials: true,
+      });
+      setUserData(result.data);
+      console.log("Current User Data:", result.data);
+    } catch (error) {
+      console.error("Error fetching current user data:", error);
+    }
+  };
+  useEffect(() => {
+      handleCurrentUser();
+    }, []); 
   const value = {
     serverUrl,
     userData,
