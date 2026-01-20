e6c5ebe2238f2ef722e35d1e11238178868136a7
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-20 22:58:09 +0530

    add: implement user routes for post management including add, edit, delete, and retrieval

--- BloggingPlatform/server/routers/user.routes.js
+++ BloggingPlatform/server/routers/user.routes.js
@@ -1,9 +1,23 @@
 import express from "express";
 import isAuth from "../middleware/isAuth.js";
 import { getCurrentUser } from "../controllers/user.controllers.js";
+import {addPost} from "../controllers/user.controllers.js";
+import multer from "../middleware/multer.js";
+import {getUserPosts} from "../controllers/user.controllers.js";
+import {getThePostByID} from "../controllers/user.controllers.js";
+import {updatePostByID} from "../controllers/user.controllers.js";
+import {deletePostByID} from "../controllers/user.controllers.js";
+import {getAllPosts} from "../controllers/user.controllers.js";
 
 const userRoutes = express.Router();
 
 userRoutes.get("/current", isAuth, getCurrentUser);
+userRoutes.post("/addpost", isAuth,multer.single("postImage"),addPost);
+userRoutes.get("/myposts", isAuth, getUserPosts);
+userRoutes.get("/post/:id", isAuth ,getThePostByID);
+userRoutes.put("/editpost/:id", isAuth, multer.single("postImage"), updatePostByID);
+userRoutes.delete("/deletepost/:id", isAuth, deletePostByID);
+userRoutes.get("/allposts", getAllPosts);
+
 
 export default userRoutes;
