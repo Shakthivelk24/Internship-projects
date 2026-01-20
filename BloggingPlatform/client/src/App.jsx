5fae75bf9f0239b6e1f1dc3b050bee5b062d1fb4
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-20 23:01:26 +0530

    add: add routes for MyPost and EditPost components in App

--- BloggingPlatform/client/src/App.jsx
+++ BloggingPlatform/client/src/App.jsx
@@ -7,6 +7,8 @@ import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import CreatePost from "./pages/CreatePost";
+import MyPost from "./pages/MyPost";
+import EditPost from "./pages/EditPost";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage/>} />
         <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/mypost" element={<MyPost />} />
+        <Route path="/edit-post/:id" element={<EditPost />} />
       </Route>
     </Routes>
   );
