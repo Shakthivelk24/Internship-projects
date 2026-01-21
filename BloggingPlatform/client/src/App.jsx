2137752d78153a29fde4b6eb283aac42079a1889
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-21 19:59:19 +0530

    refactor: update routing logic to conditionally render components based on user authentication

--- BloggingPlatform/client/src/App.jsx
+++ BloggingPlatform/client/src/App.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-import Post from "./components/Post";
-import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import IndexPage from "./pages/IndexPage";
@@ -9,19 +6,27 @@ import RegisterPage from "./pages/RegisterPage";
 import CreatePost from "./pages/CreatePost";
 import MyPost from "./pages/MyPost";
 import EditPost from "./pages/EditPost";
+import { useContext } from "react";
+import { userDataContext } from "./context/UserContext";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const {userData} = useContext(userDataContext);
   return (
+    <>
+    <Toaster />
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage/>} />
-        <Route path="/create-post" element={<CreatePost />} />
-        <Route path="/mypost" element={<MyPost />} />
-        <Route path="/edit-post/:id" element={<EditPost />} />
+        <Route path="/create-post" element={userData ? <CreatePost /> : <LoginPage />} />
+        <Route path="/mypost" element={userData ? <MyPost /> : <LoginPage />} />
+        <Route path="/edit-post/:id" element={userData ? <EditPost /> : <LoginPage />} />
       </Route>
     </Routes>
+    
+   </> 
   );
 }
 
