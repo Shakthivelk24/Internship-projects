2d2b17ce5cab3980cca353cb86f5246866d0d0ae
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-22 19:38:23 +0530

    refactor: clean up MyPost component code for improved readability and consistency

--- BloggingPlatform/client/src/pages/MyPost.jsx
+++ BloggingPlatform/client/src/pages/MyPost.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect,useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { userDataContext } from "../context/UserContext";
 import axios from "axios";
 import UserPost from "../components/UserPost";
@@ -6,21 +6,24 @@ import { useLocation } from "react-router-dom";
 
 export default function MyPost() {
   const { serverUrl, post, setPost } = useContext(userDataContext);
-  const location = useLocation(); // to track path changes
-  const [loading, setLoading] = useState(false); 
+  const location = useLocation();
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const fetchUserPosts = async () => {
       try {
         setLoading(true);
+
         const res = await axios.get(serverUrl + "/api/user/myposts", {
           withCredentials: true,
         });
-        // if backend sends { posts: [...] } adjust here
+
         const data = Array.isArray(res.data) ? res.data : res.data.posts;
         setPost(data);
+
         setLoading(false);
       } catch (error) {
-        console.log("Error fetching user posts:", error.message);
+        console.log("Load error :", error.message);
         setLoading(false);
       }
     };
@@ -28,14 +31,16 @@ export default function MyPost() {
     if (serverUrl) {
       fetchUserPosts();
     }
-  }, [serverUrl, location.pathname]); // runs every time you open MyPost
+  }, [serverUrl, location.pathname, setPost]);
 
   return (
     <>
       {loading ? (
         <p className="text-center">Loading...</p>
       ) : Array.isArray(post) && post.length > 0 ? (
-        post.map((item) => <UserPost key={item._id} post={item} />)
+        post.map((item) => (
+          <UserPost key={item._id} post={item} setPost={setPost} />
+        ))
       ) : (
         <p className="no-posts text-center mt-2">No posts found.</p>
       )}
