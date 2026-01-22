8fa7335d6832bd9ec625e4b14cb06861efc3d73c
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-22 19:38:01 +0530

    refactor: enhance AllPost component with pagination and search functionality

--- BloggingPlatform/client/src/pages/AllPost.jsx
+++ BloggingPlatform/client/src/pages/AllPost.jsx
@@ -2,11 +2,18 @@ import { useState, useEffect, useContext } from "react";
 import { userDataContext } from "../context/UserContext";
 import axios from "axios";
 import Post from "../components/Post";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function AllPost() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const { serverUrl, userData } = useContext(userDataContext);
+  const [totalPages, setTotalPages] = useState(1);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const query = new URLSearchParams(location.search);
+  const search = query.get("search") || "";
+  const page = parseInt(query.get("page")) || 1;
   useEffect(() => {
     const fetchAllPosts = async () => {
       setLoading(true);
@@ -22,13 +29,17 @@ export default function AllPost() {
       }
     };
     const fetchAllPostsUser = async () => {
+      setLoading(true);
       try {
         const response = await axios.get(`${serverUrl}/api/user/allposts`, {
           withCredentials: true,
         });
         setPosts(response.data);
+        setLoading(false);
+
       } catch (error) {
         console.log("Error fetching all posts for user:", error.message);
+        setLoading(false);
       }
     };
     if (userData) {
@@ -37,6 +48,32 @@ export default function AllPost() {
       fetchAllPosts();
     }
   }, [userData, serverUrl]);
+    
+    useEffect(() => {
+  const userload = async () => {
+    const res = await axios.get(
+      serverUrl + "/api/user/search?search=" + search + "&page=" + page , {
+        withCredentials: true,
+      }
+    );
+    setPosts(res.data.posts);
+    setTotalPages(res.data.totalPages);
+  };
+
+  const load = async () => {
+    const res = await axios.get(
+      serverUrl + "/api/user/search/public?search=" + search + "&page=" + page
+    );
+    setPosts(res.data.posts);
+    setTotalPages(res.data.totalPages);
+  };
+  if (userData) {
+    userload();
+  } else {
+    load();
+  }
+}, [search, page, userData]);
+
   return (
     <>
       {loading ? (
